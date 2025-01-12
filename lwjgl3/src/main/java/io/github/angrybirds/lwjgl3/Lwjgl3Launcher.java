b497e7e26ab5ea73c9d565d93dc1aa32480cb4f1
sanjeevgoyat02 <sanjeevgoyat094@gmail.com>
2025-01-12 23:20:58 +0530

    Add files via upload

---
+++ lwjgl3/src/main/java/io/github/angrybirds/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,25 @@
+package io.github.angrybirds.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import org.angry.controller.AngryBirds;
+
+public class Lwjgl3Launcher {
+    public static void main(String[] args) {
+        if (StartupHelper.startNewJvmIfRequired()) return;
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new AngryBirds(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("AngryBirds");
+        configuration.useVsync(true);
+        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
+        configuration.setWindowedMode(1280, 560);
+        return configuration;
+    }
+}
